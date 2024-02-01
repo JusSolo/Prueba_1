e7cd9b1174a5279ff1ea3e0bd1ae86bffd5f495c
JusSolo <Sol201598@uvg.edu.gt>
2024-02-01 10:43:06 -0600

    Creacion del proyecto

---
+++ src/main/java/grupo_12/App.java
@@ -0,0 +1,13 @@
+package grupo_12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App untitle
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
